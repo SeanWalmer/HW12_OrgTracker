4508f0cdb5c8502e604ba68503dfe827a5d88240
SeanW <sean.walmer@outlook.com>
2020-06-03 22:48:03 -0400

    Added the add dept and role features. also fixed the render roles table to show dept names instead of just the ids. also added the update role deature and fixed a typo in the Schema file.

--- app.js
+++ app.js
@@ -11,19 +11,20 @@ var connection = mysql.createConnection({
     password: "MSWatch!",
     database: "business_db"
 });
+// -----------------------------------------------------
 // Starts application with welcome message
 function startApp() {
     console.log(`\nWelcome! Accessing your Org employee database... \nWhat would you like to do first?`);
     chooseAction();
 };
-//--------------------------------------------------------
+// choose what action you would like to perform
 function chooseAction() {
     const questions = [
         {
             type: 'list',
             name: 'type',
             message: 'Please select your next action',
-            choices: ['View current Org data', 'Add a role/department/employee', 'Update employee role']
+            choices: ['View current Org data', 'Add a role/department/employee', 'Update employee role', 'quit']
         },
     ]
     // Run the employee build type based on response
@@ -32,9 +33,76 @@ function chooseAction() {
             renderTable();
         } else if (answers.type === 'Add a role/department/employee') {
             addData();
-        } else {
+        } else if(answers.type === 'Update employee role'){
             updateEmployee();
+        }else{
+            end();
         }
+    });
+};
+function end(){
+    console.log(`\nGoodbye!\n`)
+    connection.end;
+};
+// -----------------------------------------------------
+// update and employee
+function updateEmployee() {
+    connection.query(`SELECT * FROM employee`, function (err, res) {
+        let employees = res
+        let employeeNames = res.map(function(employee){
+            return `${employee.first_name} ${employee.last_name}`;
+        });
+        connection.query(`SELECT * FROM role`, function (err, res) {
+            if (err) throw err;
+            let roles = res;
+            let roleTitles = res.map(function(role){
+                return role.title
+            });
+            const questions = [
+                {
+                    type: 'list',
+                    name: 'employee',
+                    message: 'Who would you like to promote?',
+                    choices: employeeNames
+                },
+                {
+                    type: 'list',
+                    name: 'role',
+                    message: 'What is their new role?',
+                    choices: roleTitles
+                }
+            ]
+            inquirer.prompt(questions).then(answers => {
+                let roleID = 0
+                let employeeID = 0
+                for(var i = 0; i < roles.length; i++){
+                    if(answers.role === roles[i].title){
+                        roleID = roles[i].id
+                    };
+                };
+                for(var i = 0; i < employees.length; i++){
+                    if(answers.employee === `${employees[i].first_name} ${employees[i].last_name}`){
+                        employeeID = employees[i].id
+                    };
+                };
+                connection.query(
+                    "UPDATE employee SET ? WHERE ?",
+                    [
+                      {
+                        role_id: roleID
+                      },
+                      {
+                        id: employeeID
+                      }
+                    ],
+                    function(error) {
+                      if (error) throw err;
+                      console.log("Bid placed successfully!");
+                      chooseAction();
+                    }
+                  );
+            });
+        });
     });
 };
 // -----------------------------------------------------
@@ -59,7 +127,7 @@ function addData(){
         }
     });
 }
-// 
+// add an employee to database
 function addEmployee(){
     connection.query(`SELECT * FROM role`, function (err, res) {
         if (err) throw err;
@@ -122,7 +190,7 @@ function addEmployee(){
                         roleID = roles[i].id
                     };
                 };
-                for(var i = 0; i < roles.length; i++){
+                for(var i = 0; i < managers.length; i++){
                     if(answers.manager === `${managers[i].first_name} ${managers[i].last_name}`){
                         managerID = managers[i].id
                     };
@@ -144,14 +212,98 @@ function addEmployee(){
         });
     });
 };
-// 
-// function addDepartment(){
-
-// };
-// 
-// function addRole(){
-
-// };
+// add department to database
+function addDepartment(){
+    const questions = [
+        {
+            type: 'input',
+            name: 'department',
+            message: `Department Name: `,
+            validate: function (value) {
+                if (value === '') {
+                    return 'please enter a department';
+                } else {
+                    return true;
+                };
+            },
+        }
+    ]
+    inquirer.prompt(questions).then(answers => {
+        connection.query(
+            `INSERT INTO department SET ?`,
+            {
+                dept_name: answers.department
+            },
+            function (err, res) {
+                console.log('\ndepartment added!\n')
+                chooseAction();
+            }
+        );
+    });
+};
+// add role to database
+function addRole(){
+    connection.query(`SELECT * FROM department`, function (err, res) {
+        if (err) throw err;
+        let depts = res;
+        let deptNames = res.map(function(dept){
+            return dept.dept_name
+        });
+        const questions = [
+            {
+                type: 'input',
+                name: 'roleTitle',
+                message: `Role Title: `,
+                validate: function (value) {
+                    if (value === '') {
+                        return 'please enter a title';
+                    } else {
+                        return true;
+                    };
+                },
+            },
+            {
+                type: 'input',
+                name: 'salary',
+                message: `Salary: `,
+                validate: function (value) {
+                    if (value === '') {
+                        return 'please enter a salary';
+                    } else {
+                        return true;
+                    };
+                },
+            },
+            {
+                type: 'list',
+                name: 'department',
+                message: 'What department does the role belong to?',
+                choices: deptNames
+            },
+            
+        ]
+        inquirer.prompt(questions).then(answers => {
+            let deptID = 0
+            for(var i = 0; i < depts.length; i++){
+                if(answers.department === depts[i].dept_name){
+                    deptID = depts[i].id
+                };
+            };
+            connection.query(
+                `INSERT INTO role SET ?`,
+                {
+                    title: answers.roleTitle,
+                    salary: parseInt(answers.salary),
+                    department_id: deptID,
+                },
+                function (err, res) {
+                    console.log('\nrole added!\n')
+                    chooseAction();
+                }
+            );
+        });
+    });
+};
 // -----------------------------------------------------
 // Decides what data to render
 function renderTable() {
@@ -200,16 +352,20 @@ function showDepartments() {
 };
 // Render roles to table
 function showRoles() {
-    connection.query(`SELECT * FROM role`, function (err, res) {
+    connection.query(`SELECT title, salary, dept_name 
+     FROM role
+     INNER JOIN department ON role.department_id = department.id 
+     ORDER BY department.id ASC`, function (err, res) {
         if (err) throw err;
         console.log('\n');
         console.table(res);
-        console.log(res);
+        // console.log(res);
         console.log('\n');
         chooseAction()
     });
 };
 // -----------------------------------------------------
+// failed matchID function to be reused becase MatchProp wont be added as a property and it just looks for the literal property of matchProp =(
 // function matchID(value, matchArray, matchProp){
 //     for(var i = 0; i < matchArray.length; i++){
 //         if(value === matchArray[i].matchProp){
